08e5a9e40b2032aa58a733c876f68f314e9bbdbf
Ayush Rawat <ayushrawat11562@gmail.com>
2020-10-25 22:16:26 +0530

    moved js

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function loadFile() {
-    var http = new XMLHttpRequest();
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
 
-    http.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementsById("container").innerHTML = this.responseText;
+            myFunction(this);
         }
     };
-
-    http.open("GET", "ajax.txt", true);
-    http.send();
+    xhttp.open("GET", "catalog.xml", true);
+    xhttp.send();
+}
+function myFunction(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>Artist</th><th>Title</th><th>Country</th></tr>";
+    var x = xmlDoc.getElementsByTagName("CD");
+    for (i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+            x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName('COUNTRY')[0].childNodes[0].nodeValue +
+            "</td></tr>"
+    }
+    document.getElementById("demo").innerHTML = table;
 }
